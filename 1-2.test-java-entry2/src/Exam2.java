38d451fbfdfa55fb7bf42347e5540b47de2875cc
Ishida Honoka <honoka.ishida11@gmail.com>
2026-01-16 11:10:57 +0900

    nyumon2

---
+++ 1-2.test-java-entry2/src/Exam2.java
@@ -0,0 +1,14 @@
+public class Exam2 {
+    public static void main(String[] args) {
+        
+        int i = 1 ;
+        String name = "石田穂野歌";
+
+        while(i <= 10){
+            System.out.println( i + "." + name);
+
+            i++;
+        }
+    }
+
+}
